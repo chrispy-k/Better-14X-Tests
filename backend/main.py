6dd00082d68de1d34a8d722985f337513f4a2073
Carter Peyton <carter.d.peyton@gmail.com>
2021-10-23 12:51:54 -0700

    local postgres setup w/ barebone user model

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,24 @@
 from flask import Flask
-from flask_restful import Api, Resource 
-
+from flask_restful import Api, Resource, reqparse 
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/better_143'
+
+db = SQLAlchemy(app)
 api = Api(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
-# User class 
-class User(Resource):
-    def get(self):
-        return {
-            'data': 'user test'
-        }
-
-    def post(self, username, email, password):
-        return {
-            'username': username,
-            'email': email,
-            'password': password
-        }
-
-api.add_resource(User, "/register/<string:username>/<string:email>/<string:password>")
+    def __repr__(self):
+        return f'User: {self.username}, Email: {self.email}'
 
 
 if __name__ == "__main__":
--- backend/test.py
+++ backend/test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "/register/carter/c@c.com/123")
+response = requests.put(BASE + "/register/carter/c@c.com/123")
 print(response.json())
