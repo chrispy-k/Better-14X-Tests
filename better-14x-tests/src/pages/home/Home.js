b68548e8f6289ddbd05de1748818b6f755e52273
Chris Kim <chrisk@veronica-2.local>
2021-10-23 16:54:26 -0700

    login and register

--- better-14x-tests/src/pages/home/Home.js
+++ better-14x-tests/src/pages/home/Home.js
@@ -1,7 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useHistory } from 'react-router-dom'
+import "./Login.css";
 
-export default function Home() {
-    return <div>
-        <p> We made a home page wohooo</p>
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  function toRegisterPage() {
+      history.push('/Register');
+  }
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+        <Button block size="lg" onClick={toRegisterPage}>
+          Register
+        </Button>
+      </Form>
     </div>
+  );
 }
