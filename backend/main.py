8f9420e96bdc38a280b51b2b20ff5c984a88af30
Carter Peyton <carter.d.peyton@gmail.com>
2021-10-23 13:52:18 -0700

    Username/pw and token authentication added along w/ test for protected resource

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, abort, jsonify, g, url_for
 from flask_restful import Api, Resource, reqparse 
 from flask_sqlalchemy import SQLAlchemy 
+from flask_httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context 
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/better_143'
+app.config['SECRET_KEY'] = 'a b c d e'
 
 db = SQLAlchemy(app)
 api = Api(app)
@@ -23,6 +27,23 @@ class User(db.Model):
     # returns true if the password matches the hash
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+
+
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = User.query.get(data['id'])
+        return user
     
     def __init__(self, username, email):
         self.username = username
@@ -33,7 +54,7 @@ class User(db.Model):
 
 # registers a new User object
 @app.route('/api/auth/register', methods=['POST'])
-def new_user():
+def register():
     username = request.json.get('username')
     password = request.json.get('password')
     email = request.json.get('email')
@@ -63,6 +84,40 @@ def get_user(id):
     if not user:
         abort(400)
     return jsonify({'username': user.username})
+
+
+auth = HTTPBasicAuth()
+
+# tmp api endpoint that requires authentication to get the resource
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return {
+        'data': 'Hello, %s!' % g.user.username
+    }
+
+@app.route('/api/auth/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return {
+        'token': token.decode('ascii')
+    }
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # try token auth first
+    user = User.verify_auth_token(username_or_token)
+    
+    # if token auth fails try username/pw
+    if not user:
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+
+    g.user = user
+    return True
 
 
 if __name__ == "__main__":
