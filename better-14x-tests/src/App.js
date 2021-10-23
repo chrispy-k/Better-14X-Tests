0199408decbb1c13a92e19915498ceff0e5dadd1
Alex Chan <chana98@uw.edu>
2021-10-23 15:49:35 -0700

    created a test home page

--- better-14x-tests/src/App.js
+++ better-14x-tests/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "./Home";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Router>
+          <Route path="/" component={Home} />
+        </Router>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +22,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
