dd29e711f0a4b6d1efd140a7d46861921c3d7f02
Chris Kim <chrisk@veronica-2.local>
2021-10-23 17:15:24 -0700

    register page

--- better-14x-tests/src/pages/home/Register.js
+++ better-14x-tests/src/pages/home/Register.js
@@ -1,9 +1,81 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Register() {
-    return (
-        <h1>hi</h1>
-    )
+  return (
+    <Form>
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalFirstName">
+        <Form.Label column sm={2}>
+          First Name
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="text" placeholder="Your name" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalLastName">
+        <Form.Label column sm={2}>
+          Last Name
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="text" placeholder="Last Name" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>
+          Email
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="email" placeholder="email" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+        <Form.Label column sm={2}>
+          Password
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="password" placeholder="password" />
+        </Col>
+      </Form.Group>
+
+      <fieldset>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={2}>
+            Radios
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Check
+              type="radio"
+              label="Student"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="radio"
+              label="TA"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios2"
+            />
+            <Form.Check
+              type="radio"
+              label="Professor"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            />
+          </Col>
+        </Form.Group>
+      </fieldset>
+
+      <Form.Group as={Row} className="mb-3">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Button type="submit">Sign in</Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
 }
