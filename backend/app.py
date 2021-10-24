2139e771b205c630474f257f6a82f761704859ca
Carter Peyton <carter.d.peyton@gmail.com>
2021-10-24 00:07:40 -0700

    m

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rbcenvavwhsjsk:277a11032fbf27c60a93707aa7a40fd8e33361d5601cd3a1d98d63bcb7d53dff@ec2-3-219-103-45.compute-1.amazonaws.com:5432/d2ailsp0qf4tui'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rbcenvavwhsjsk:277a11032fbf27c60a93707aa7a40fd8e33361d5601cd3a1d98d63bcb7d53dff@ec2-3-219-103-45.compute-1.amazonaws.com:5432/d2ailsp0qf4tui'
 app.config['SECRET_KEY'] = 'a b c d e'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
