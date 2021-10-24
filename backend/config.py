3c81c6cae1c696b0556fb068e07bc94fd21f3061
Carter Peyton <carter.d.peyton@gmail.com>
2021-10-24 00:03:52 -0700

    sqlalc

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,4 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgres://rbcenvavwhsjsk:277a11032fbf27c60a93707aa7a40fd8e33361d5601cd3a1d98d63bcb7d53dff@ec2-3-219-103-45.compute-1.amazonaws.com:5432/d2ailsp0qf4tui'
     SECRET_KEY = 'a b c d e'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
