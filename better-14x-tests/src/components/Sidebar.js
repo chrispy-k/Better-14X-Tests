d60f1ea152c9a8f2a618c22dc3d881fd075a8f12
Alex Chan <chana98@uw.edu>
2021-10-23 21:29:30 -0700

    updated dashboard page

--- better-14x-tests/src/components/Sidebar.js
+++ better-14x-tests/src/components/Sidebar.js
@@ -1,37 +1,55 @@
-import React from "react";
-import {Nav} from "react-bootstrap";
-import { withRouter } from "react-router";
-import '../pages/css/dashboard.css';
-
-const Side = props => {
-   
-
-    return (
-        <>
-    
-            <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
-            activeKey="/home"
-            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-            >
-                <div className="sidebar-sticky"></div>
-            <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                Disabled
-                </Nav.Link>
-            </Nav.Item>
-            </Nav>
-          
-        </>
-        );
-  };
-  const Sidebar = withRouter(Side);
-  export default Sidebar
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+ 
+const Sidebar = () => {
+  return (
+    <div
+      style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
+    >
+      <CDBSidebar textColor="#fff" backgroundColor="lightblue">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a
+            href="/"
+            className="text-decoration-none"
+            style={{ color: 'inherit' }}
+          >
+            CSE Tests
+          </a>
+        </CDBSidebarHeader>
+ 
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+          <NavLink exact to="/profile" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Profile</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/courses" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="book">
+                Courses
+              </CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/tables" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="percent">Score</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/analytics" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="cog">
+                Settings
+              </CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+      </CDBSidebar>
+    </div>
+  );
+};
+ 
+export default Sidebar;
--- better-14x-tests/src/pages/dashboard/dashboard.js
+++ better-14x-tests/src/pages/dashboard/dashboard.js
@@ -1,35 +1,54 @@
 import React from 'react';
+import Sidebar from '../../components/Sidebar.js'
 import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { withRouter } from "react-router";
-import Sidebar from "../../components/Sidebar.js";
 import '../css/dashboard.css';
-const Dash = props => {
+export default function Dashboard() {
     return (
-        <>
-         <Container fluid>
-         <p>Dashboard</p>
-         {/* make this a component */}
-         <div class="card" style={{width: '18rem;'}}>
-            <div class="card-body">
-                <h5 class="card-title">CSE 142</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Midterm 1</h6>
-                <p class="card-text">Going over content in weeks 1-5.</p>
-                <a href="#" class="card-link">Omegalul</a>
-                <a href="#" class="card-link">Get rekted</a>
+    <div>
+        <div class ="flex-container">
+            <div class = "flex-child 1">
+                <Sidebar />
+            </div>
+            <div class = "flex-child2">
+            <Card style={{ width: '18rem', borderRadius: '25px'}}>
+            <Card.Body>
+                <Card.Title>CSE 142</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Midterm 1</Card.Subtitle>
+                <Card.Text>
+                    Going over content in weeks 1-5.
+                </Card.Text>
+                <Card.Link href="#">Test Link</Card.Link>
+            </Card.Body>
+        </Card>
+            </div>
+            <div class = "flex-child2">
+            <Card style={{ width: '18rem', borderRadius: '25px' }}>
+            <Card.Body>
+                <Card.Title>CSE 143</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Midterm 1</Card.Subtitle>
+                <Card.Text>
+                    Going over content in weeks 1-5.
+                </Card.Text>
+                <Card.Link href="#">Test Link</Card.Link>
+            </Card.Body>
+        </Card>
+            </div>
+            <div class = "flex-child2">
+            <Card style={{ width: '18rem', borderRadius: '25px'}}>
+            <Card.Body>
+                <Card.Title>CSE 373</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Midterm 1</Card.Subtitle>
+                <Card.Text>
+                    Going over content in weeks 1-5.
+                </Card.Text>
+                <Card.Link href="#">Test Link</Card.Link>
+            </Card.Body>
+        </Card>
             </div>
         </div>
-                <Row>
-                    <Col xs={2} id="sidebar-wrapper">      
-                      <Sidebar />
-                    </Col>
-                    <Col  xs={10} id="page-content-wrapper">
-                        this is a test
-                    </Col> 
-                </Row>
 
-            </Container>
-        </>
-        );
-  };
-  const Dashboard = withRouter(Dash);
-  export default Dashboard
+        {/* make this a component and clickable*/}
+    </div>
+    )
+};
