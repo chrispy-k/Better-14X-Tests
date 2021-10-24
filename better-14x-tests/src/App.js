b8ecbdd0efb01efa2413b2fd27ed328e834ca7ff
Chris Kim <chrisk@veronica-2.local>
2021-10-24 01:37:49 -0700

    merge conflict resolved

--- better-14x-tests/src/App.js
+++ better-14x-tests/src/App.js
@@ -4,12 +4,8 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './pages/home/Home';
 import Register from './pages/home/Register';
 import Dashboard from './pages/dashboard/dashboard';
-<<<<<<< HEAD
-
-=======
 import Test from './pages/tests/test';
 import Profile from './pages/dashboard/profile';
->>>>>>> c50e981e7a3673a999e06ce9dce2fd80ca557b9c
 
 function App() {
   return (
@@ -19,13 +15,9 @@ function App() {
           <Switch>
             <Route path="/" exact component={() => <Home />} />
             <Route path="/register" component={() => <Register />} />
-<<<<<<< HEAD
-            <Route path="/dashboard" component={() => <Dashboard />} />
-=======
             <Route path="/profile" component={() => <Profile />} />
             <Route path="/dashboard" component={() => <Dashboard />} />
             <Route path="/test" component={() => <Test />} />
->>>>>>> c50e981e7a3673a999e06ce9dce2fd80ca557b9c
           </Switch>
         </Router>
       </header>
