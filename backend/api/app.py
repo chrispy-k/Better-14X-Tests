28d4c35f03596c75168eae8130c8d7dbdc61e92d
Carter Peyton <carter.d.peyton@gmail.com>
2021-10-23 22:43:35 -0700

    f

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -13,13 +12,13 @@ from auth_routes import *
 from test_routes import * 
 
 # Init db from the models.py file 
-db.init_app(app)
+""" db.init_app(app)
 
 # comment out when don't want to wipe db
 with app.app_context():
-    db.create_all()
+    db.create_all() """
 
-migrate = Migrate(app, db) # this
+#migrate = Migrate(app, db) # this
 
 if __name__ == "__main__":
     app.run(debug=True)
